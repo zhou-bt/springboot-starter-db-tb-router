c66b5e16bc9de5cd862ab1684c9994c3ca29ee3b
zbt <zbt98_11@163.com>
2023-06-12 20:31:03 +0800

    delete massage

--- src/main/java/cn/bugstack/middleware/db/router/DBRouterJoinPoint.java
+++ src/main/java/cn/bugstack/middleware/db/router/DBRouterJoinPoint.java
@@ -34,14 +34,6 @@ public class DBRouterJoinPoint {
     public void aopPoint() {
     }
 
-    /**
-     * 所有需要分库分表的操作，都需要使用自定义注解进行拦截，拦截后读取方法中的入参字段，根据字段进行路由操作。
-     * 1. dbRouter.key() 确定根据哪个字段进行路由
-     * 2. getAttrValue 根据数据库路由字段，从入参中读取出对应的值。比如路由 key 是 uId，那么就从入参对象 Obj 中获取到 uId 的值。
-     * 3. dbRouterStrategy.doRouter(dbKeyAttr) 路由策略根据具体的路由值进行处理
-     * 4. 路由处理完成比，就是放行。 jp.proceed();
-     * 5. 最后 dbRouterStrategy 需要执行 clear 因为这里用到了 ThreadLocal 需要手动清空。关于 ThreadLocal 内存泄漏介绍 https://t.zsxq.com/027QF2fae
-     */
     @Around("aopPoint() && @annotation(dbRouter)")
     public Object doRouter(ProceedingJoinPoint jp, DBRouter dbRouter) throws Throwable {
         String dbKey = dbRouter.key();
